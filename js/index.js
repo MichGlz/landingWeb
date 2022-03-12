19873ea13400251761f55717058f2e8a4fdb415b
Miguel <migu0403@stud.kea.dk>
2022-03-12 01:41:08 +0100

    clean form

--- js/index.js
+++ js/index.js
@@ -2,22 +2,25 @@
 
 const star = () => {
   const btnSubmit = document.querySelector(".btn");
+  const inputName = document.querySelector("#customer-name");
+  const inputEmail = document.querySelector("#customer-email");
+  const checkBox = document.querySelector('[type="checkbox"]');
   btnSubmit.addEventListener("click", (event) => {
     event.preventDefault();
-    const payLoad = formInfo();
-    postData(payLoad);
+    const payLoad = formInfo(inputName, inputEmail);
+    postData(payLoad, inputName, inputEmail, checkBox);
   });
 };
 
-const formInfo = () => {
+const formInfo = (inputName, inputEmail) => {
   const customer = {};
-  customer.name = document.querySelector("#customer-name").value;
-  customer.email = document.querySelector("#customer-email").value;
+  customer.name = inputName.value;
+  customer.email = inputEmail.value;
   console.log(customer);
-  return JSON.stringify(customer);
+  return customer;
 };
 
-const postData = (payLoad) => {
+const postData = (payLoad, inputName, inputEmail, checkBox) => {
   //   console.log(payLoad);
   fetch("https://reicpe-9cc2.restdb.io/rest/babybox", {
     method: "POST",
@@ -25,12 +28,16 @@ const postData = (payLoad) => {
       "x-apikey": "606d5dcef5535004310074f4",
       "Content-Type": "application/json",
     },
-    body: payLoad,
+    body: JSON.stringify(payLoad),
   })
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      alert("your mail was save");
+      alert(`${payLoad.name} thank you for your preference!
+       your email was save`);
+      inputName.value = "";
+      inputEmail.value = "";
+      checkBox.checked = false;
     })
     .catch((err) => {
       console.error(err);
